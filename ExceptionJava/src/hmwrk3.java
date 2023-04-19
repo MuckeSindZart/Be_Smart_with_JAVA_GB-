a11df1f92c87da082d58ad26a39a426e7ed8bf50
MuckeSindZart <meinekomarov@gmail.com>
2023-04-19 16:25:11 +0600

    ExceptionHmwrk03.1

--- ExceptionJava/src/hmwrk3.java
+++ ExceptionJava/src/hmwrk3.java
@@ -51,7 +51,7 @@ public class hmwrk3 {
         int sum = 0;
 
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
+            for (int j = 0; j < array.length; j++) {
                 try {
                     sum += Integer.parseInt(array[i][j]);
                 } catch (NumberFormatException e) {
